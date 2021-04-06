ce902fd7502022907ca0cd046165344d641ff0c0
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2021-04-06 16:46:22 +0700

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -4,7 +4,6 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '<Your Mysql Password>'
 app.config['MYSQL_USER'] = '<Your MySQL Username>'
 app.config['MYSQL_DB'] = '<Your MySQL database name>'
