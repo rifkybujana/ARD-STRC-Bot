b3550fd7a626ca3e22cb29fe026a79243bc34b69
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2021-06-08 20:06:39 +0700

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Ipan218ok'
+app.config['MYSQL_PASSWORD'] = #PASSWORD
 app.config['MYSQL_DB'] = 'strc'
 
 mysql = MySQL(app)
